e6480047b92e1bffc4338c85296e15bcba1ccf0e
demoblox-xyz <79365870+demoblox-xyz@users.noreply.github.com>
2021-02-25 19:45:07 +0000

    Landing page added
    just because

---
+++ landing.php
@@ -0,0 +1,206 @@
+ <form name="aspnetForm" method="post" action="default.aspx" id="aspnetForm">
+<input type="hidden" name="__VIEWSTATE" id="__VIEWSTATE" value="cGzC+UmxBWd9Uhsy50YZRKz95LnC7oOJP3rWREHTGm0nP21f+r8G2WJWj+3yb0g9Ihw38KwfI9Q7OmgSbkgfjCEr92fZIM4sGVViY+MfndQ0eBdDcchlkMti/n2ge22kNAa4QehONhTj8GfH9fP6d2R2oyq1mc8x29Kmm1fiihztIBeN3edT/zTsubWzb5mt+MNjR9gdtwGAwyYFQNIK27S9Pljw2WEKZw9VaI8T9OcHu2nMnk8jsiRSCa2j6lVm6u4zJuyISgonrt3thhmwB4ZSdpNnJonJhPYBfeMxO8cb3eUgMR9BA5iik3MxcqzhX5LFKpXXnlRcT3/EXf1laateolBQuQBxUn0WknS7M95l957M7dS5xJlMPgdTJjKNKq8fI5imT2WqD7x7SM2NXbsd6RFc5Iy0cUMasucBZCFFdJ2mxnXh6n6nW6M7rCwDapldZ6nbkyMZOr6JHDx176MW5oHEBrCufTuIM/mWs0IGguUzlQfkVzRPEAKVW/I44jjWzMunBWtHF/ghdA4Dhd1/0et+kfHOB3+Q0QckQiAZRt2XwQDphPRl2ZehYHkO9sys2JNj6sH2hhxd+5J5i1eIrWz2M8w2UQHI9T8jJMlSUxCRH5yVYkBI7MkW7aboUjT2weQR2QPYlT+JInJpUxMRS3XrQzRWJN+Wwny0mHJL4/iHeZyNT+Lis1fI1QNKrPYcXYJkRKEcnURffp8wNJS4QqFOML3umi3FWPzTuU+DC/ftNKvhv80GRsw7Yk9dsqFwQb01uiYsKemi+f5FR4vnlcIjqDzcD6pAdtq1pYmCaibUhPLHEh++FKo+SN24R1aRKxIZeW9IWmQDlz/qE6uLuG2bFKYUPi1pV1Om2Y7jwyZluA6e/j1WqO4TojmuV1dLj9x8TNEXG9cgayBQPLr8vPiM0Fp52oUGFJKBNCDaVI9sZwihP+Ujj2SNX6B0oXTl2WJy5W58el7uJJvD4Euw4imRBA5XcL4p+VE0BP2k48kucS6CHLtjtnaNYTnhC9TBDMS0KnLoSBtFLxJnoMrTxcACyDGKEcYpwvIkJXEALgzJdYprUzpp7PI11POe0iwNXIhRr6tPUOVGkioP8mN+GjAJKbMfItlfUCSC3ogV9pnq8M0aVib06CeJH1zZYCMCUHvyccePCOofsgCyKJJRLWBwxGsiSv+vjcPQio7VR2TfmqJVUqXl3JA4Ck4PgkSJgrS3SIH7XRMZyLiFvlf0lHDt07uU+yWN7VdqGkHmsyQ4Z/QqjJC8lOMpbCHqAFqFmat1bYrkE369N4MbbMrKSy84UL63shrhAo1oRwk+N60DydLKU46tMc3eUXu671LsNu07hMo9RzHux3quFcvyDvkXf/s390v7C3XJuiQ="/>
+
+<link rel="stylesheet" type="text/css" href="People.css" media="screen"/>
+
+<link rel="icon" href="/images/favicon.ico?" type="image/x-icon">
+
+        <div align="center">
+            <table cellspacing="0" cellpadding="0" width="800">
+                <tr>
+                    <td style="clear: none; border-right: black 1px solid; border-left: black 1px solid" class="Header" align="center">
+                        <div id="_ctl0_Panel2" class="Banner">
+                            
+                            
+                            
+
+	<title>DEMOBLOX</title>
+                            <table cellspacing="0" cellpadding="0" width="100%">
+                                <tr>
+                                 
+                                    <td align="center" width="300">
+                                        <img src="banner.png" alt="banner" width="1150" height="95">
+                                    </td>
+                                    <td align="" width="250">
+                                        
+                                </tr>
+                            </table>
+                        
+</div>
+                    </td>
+                </tr>
+                <tr>
+                    <td align="center" style="border-right: black 1px solid; padding-right: 8px; padding-left: 8px;
+                        padding-bottom: 0px; border-left: black 1px solid; padding-top: 0px; border-bottom: black 1px solid;
+                        background-color: lightsteelblue">
+                        <span class="MenuItem">
+                            <a  id="_ctl0_HyperlinkHome" class="MenuItem" href="https://demo-blox.000webhostapp.com/testingtesting123.html">Home</a>&nbsp;|&nbsp;<a id="_ctl0_HyperLink10" class="MenuItem" href="https://demo-blox.000webhostapp.com/People.php">Browse</a>&nbsp;|
+                            <a  id="_ctl0_HyperLink3" class="MenuItem" href="https://demo-blox.000webhostapp.com/testingtesting123.html">Multiplayer</a>
+                            |&nbsp;<a  id="_ctl0_HyperLink11" class="MenuItem" href="https://demo-blox.000webhostapp.com/testingtesting123.html">Forum</a>&nbsp;|&nbsp;<a  id="_ctl0_Hyperlink1" class="MenuItem" href="https://demoblox-xyz.fandom.com/wiki/Demoblox-xyz_Wiki">Info</a>
+                            
+                        </span>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="padding-top: 16px">
+                        <div style="text-align: center">
+                            <p>
+                            </div><div class="SystemAlert">
+						<div class="SystemAlertText" style="background-color: purple">
+							<div class="Exclamation">
+							</div>
+							<div><b style="color:white"><center>Alpha testers are cool.</center></b></div>
+						</div>
+					</div></div>				<div id="Body">    
+    <table width="100%" style="margin-top: 5px" cellspacing="10">
+        <tr>
+            <td width="60%">
+                <p class="Title">
+                    Welcome to DEMOBLOX!</p>
+                <p>
+                    To get started socialize with friends, have a good time at the weekly place, just have fun. Make whatever your mind can think of.</p>
+               
+                <p>
+                    Please enter feedback and bugs in the forums once they release next week
+                    </p>
+                   <p>Thank you from the DEMOBLOX team.</p>
+            </td>
+
+                
+      
+     
+            </td>
+            <td height="100%">
+                <table id="_ctl0_ContentPlaceHolder1_DataList2" cellspacing="0" cellpadding="8" bordercolor="Black" border="0" bgcolor="LightSteelBlue" height="100%" width="100%">
+	<tr>
+		<td colspan="3" class="Title">
+                        Newest&nbsp;People
+                    </td>
+	</tr><tr>
+		<td align="center">
+                        <a id="_ctl0_ContentPlaceHolder1_DataList2__ctl1_ContentImage2" supportsalphachannel="True" title="scrudu" href="http://demoblox.xyz/User12.php" blankurl="/Thumbs/Blank.ashx?x=48&amp;y=48" imageurl="/Thumbs/Model.ashx?x=48&amp;y=48&amp;id=1801&amp;format=Png" style="display:inline-block;BEHAVIOR:url(/web/20060703125908im_/http://roblox.com/UI/Image.htc);cursor:hand;"></a>
+                        <br>
+                        <a id="_ctl0_ContentPlaceHolder1_DataList2__ctl1_HyperLink2" class="GridLink" href="User12.php">scrudu</a>
+                        <p>
+                        </p>
+                    </td><td align="center">
+                        <a id="_ctl0_ContentPlaceHolder1_DataList2__ctl4_ContentImage2" supportsalphachannel="True" title="WIP" href="/web/20060703125908/http://roblox.com/User.aspx?id=266" blankurl="/Thumbs/Blank.ashx?x=48&amp;y=48" imageurl="/Thumbs/Model.ashx?x=48&amp;y=48&amp;id=1795&amp;format=Png" style="display:inline-block;BEHAVIOR:url(/web/20060703125908im_/http://roblox.com/UI/Image.htc);cursor:hand;"></a>
+                        <br>
+                        <a id="_ctl0_ContentPlaceHolder1_DataList2__ctl4_HyperLink2" class="GridLink" href="User11.php">credit</a>
+                        <p>
+                        </p>
+                    </td><td align="center">
+                        <a id="_ctl0_ContentPlaceHolder1_DataList2__ctl7_ContentImage2" supportsalphachannel="True" title="WIP" href="/web/20060703125908/http://roblox.com/User.aspx?id=263" blankurl="/Thumbs/Blank.ashx?x=48&amp;y=48" imageurl="/Thumbs/Model.ashx?x=48&amp;y=48&amp;id=1789&amp;format=Png" style="display:inline-block;BEHAVIOR:url(/web/20060703125908im_/http://roblox.com/UI/Image.htc);cursor:hand;"></a>
+                        <br>
+                        <a id="_ctl0_ContentPlaceHolder1_DataList2__ctl7_HyperLink2" class="GridLink" href="User10.php">theodore</a>
+                        <p>
+                        </p>
+                    </td>
+	</tr><tr>
+		<td align="center">
+                        <a id="_ctl0_ContentPlaceHolder1_DataList2__ctl2_ContentImage2" supportsalphachannel="True" title="WIP" href="/web/20060703125908/http://roblox.com/User.aspx?id=268" blankurl="/Thumbs/Blank.ashx?x=48&amp;y=48" imageurl="/Thumbs/Model.ashx?x=48&amp;y=48&amp;id=1799&amp;format=Png" style="display:inline-block;BEHAVIOR:url(/web/20060703125908im_/http://roblox.com/UI/Image.htc);cursor:hand;"></a>
+                        <br>
+                        <a id="_ctl0_ContentPlaceHolder1_DataList2__ctl2_HyperLink2" class="GridLink" href="http://demo-blox.000webhostapp.com/User2.html">cheese</a>
+                        <p>
+                        </p>
+                    </td><td align="center">
+                        <a id="_ctl0_ContentPlaceHolder1_DataList2__ctl5_ContentImage2" supportsalphachannel="True" title="administrationtest" href="/web/20060703125908/http://roblox.com/User.aspx?id=265" blankurl="/Thumbs/Blank.ashx?x=48&amp;y=48" imageurl="/Thumbs/Model.ashx?x=48&amp;y=48&amp;id=1793&amp;format=Png" style="display:inline-block;BEHAVIOR:url(/web/20060703125908im_/http://roblox.com/UI/Image.htc);cursor:hand;"></a>
+                        <br>
+                        <a id="_ctl0_ContentPlaceHolder1_DataList2__ctl5_HyperLink2" class="GridLink" href="http://demo-blox.000webhostapp.com/User1.html">administrationtest</a>
+                        <p>
+                        </p>
+                    </td><td align="center">
+                        <a id="_ctl0_ContentPlaceHolder1_DataList2__ctl8_ContentImage2" supportsalphachannel="True" title="WIP" href="/web/20060703125908/http://roblox.com/User.aspx?id=262" blankurl="/Thumbs/Blank.ashx?x=48&amp;y=48" imageurl="/Thumbs/Model.ashx?x=48&amp;y=48&amp;id=1787&amp;format=Png" style="display:inline-block;BEHAVIOR:url(/web/20060703125908im_/http://roblox.com/UI/Image.htc);cursor:hand;"></a>
+                        <br>
+                        <a id="_ctl0_ContentPlaceHolder1_DataList2__ctl8_HyperLink2" class="GridLink" href="http://demo-blox.000webhostapp.com/User6.html">Delightful</a>
+                        <p>
+                        </p>
+                    </td>
+	</tr><tr>
+		<td align="center">
+                        <a id="_ctl0_ContentPlaceHolder1_DataList2__ctl3_ContentImage2" supportsalphachannel="True" title="WIP" href="/web/20060703125908/http://roblox.com/User.aspx?id=267" blankurl="/Thumbs/Blank.ashx?x=48&amp;y=48" imageurl="/Thumbs/Model.ashx?x=48&amp;y=48&amp;id=1797&amp;format=Png" style="display:inline-block;BEHAVIOR:url(/web/20060703125908im_/http://roblox.com/UI/Image.htc);cursor:hand;"></a>
+                        <br>
+                        <a id="_ctl0_ContentPlaceHolder1_DataList2__ctl3_HyperLink2" class="GridLink" href="User.aspx?id=7">kirbyfan277</a>
+                        <p>
+                        </p>
+                    </td><td align="center">
+                        <a id="_ctl0_ContentPlaceHolder1_DataList2__ctl6_ContentImage2" supportsalphachannel="True" title="WIP" href="/web/20060703125908/http://roblox.com/User.aspx?id=264" blankurl="/Thumbs/Blank.ashx?x=48&amp;y=48" imageurl="/Thumbs/Model.ashx?x=48&amp;y=48&amp;id=1791&amp;format=Png" style="display:inline-block;BEHAVIOR:url(/web/20060703125908im_/http://roblox.com/UI/Image.htc);cursor:hand;"></a>
+                        <br>
+                        <a id="_ctl0_ContentPlaceHolder1_DataList2__ctl6_HyperLink2" class="GridLink" href="User.aspx?id=8">pastebin</a>
+                        <p>
+                        </p>
+                    </td><td align="center">
+                        <a id="_ctl0_ContentPlaceHolder1_DataList2__ctl9_ContentImage2" supportsalphachannel="True" title="WIP" href="/web/20060703125908/http://roblox.com/User.aspx?id=9" blankurl="/Thumbs/Blank.ashx?x=48&amp;y=48" imageurl="/Thumbs/Model.ashx?x=48&amp;y=48&amp;id=1785&amp;format=Png" style="display:inline-block;BEHAVIOR:url(/web/20060703125908im_/http://roblox.com/UI/Image.htc);cursor:hand;"></a>
+                        <br>
+                        <a id="_ctl0_ContentPlaceHolder1_DataList2__ctl9_HyperLink2" class="GridLink" href="User.aspx?id=9">ilovepandas647</a>
+                        <p>
+                        </p>
+                    </td>
+	</tr><tr>
+		<td colspan="3" align="right">
+                        <a id="_ctl0_ContentPlaceHolder1_DataList2__ctl10_HyperLink3" href="http://demo-blox.000webhostapp.com/People.php">Browse All People</a>
+                    </td>
+	</tr>
+</table></td>
+        </tr>
+                <a href="https://demo-blox.000webhostapp.com/User1.php">Weekly DEMOBLOX game!</a>
+
+        <tr>
+            <td colspan="2">
+                <table id="_ctl0_ContentPlaceHolder1_DataList1" cellspacing="0" cellpadding="8" bordercolor="Black" border="0" bgcolor="LightSteelBlue" height="100%" width="100%">
+	<tr>
+		<td colspan="6" class="Title">
+                        Recently Visited Places
+                    </td>
+	</tr><tr>
+		<td>
+                        <a id="_ctl0_ContentPlaceHolder1_DataList1__ctl1_ContentImage1" title="DrummerDude952's Place" href="/web/20060703125908/http://roblox.com/User.aspx?id=265" style="display:inline-block;cursor:hand;"><img src="/web/20060703125908im_/http://roblox.com/Thumbs/Level.ashx?x=120&amp;y=70&amp;id=1792&amp;format=Jpeg" border="0"/></a>
+                    </td><td>
+                        <a id="_ctl0_ContentPlaceHolder1_DataList1__ctl2_ContentImage1" title="Rige's Place" href="/web/20060703125908/http://roblox.com/User.aspx?id=269" style="display:inline-block;cursor:hand;"><img src="/web/20060703125908im_/http://roblox.com/Thumbs/Level.ashx?x=120&amp;y=70&amp;id=1800&amp;format=Jpeg" border="0"/></a>
+                    </td><td>
+                        <a id="_ctl0_ContentPlaceHolder1_DataList1__ctl3_ContentImage1" title="golin6868's Place" href="/web/20060703125908/http://roblox.com/User.aspx?id=268" style="display:inline-block;cursor:hand;"><img src="/web/20060703125908im_/http://roblox.com/Thumbs/Level.ashx?x=120&amp;y=70&amp;id=1798&amp;format=Jpeg" border="0"/></a>
+                    </td><td>
+                        <a id="_ctl0_ContentPlaceHolder1_DataList1__ctl4_ContentImage1" title="ausburne's Place" href="/web/20060703125908/http://roblox.com/User.aspx?id=267" style="display:inline-block;cursor:hand;"><img src="/web/20060703125908im_/http://roblox.com/Thumbs/Level.ashx?x=120&amp;y=70&amp;id=1796&amp;format=Jpeg" border="0"/></a>
+                    </td><td>
+                        <a id="_ctl0_ContentPlaceHolder1_DataList1__ctl5_ContentImage1" title="casey5's Place" href="/web/20060703125908/http://roblox.com/User.aspx?id=266" style="display:inline-block;cursor:hand;"><img src="/web/20060703125908im_/http://roblox.com/Thumbs/Level.ashx?x=120&amp;y=70&amp;id=1794&amp;format=Jpeg" border="0"/></a>
+                    </td><td></td>
+	</tr><tr>
+		<td colspan="6" align="right">
+                        <a id="_ctl0_ContentPlaceHolder1_DataList1__ctl6_HyperLink1" href="Browse/Places.aspx">Browse All Places</a>
+                    </td>
+	</tr>
+</table></td>
+        </tr>
+    </table>
+
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="padding-left: 8px; padding-top: 12px">
+                        
+<hr>
+<div class="Legal" style="TEXT-ALIGN: center">DEMOBLOX, "Release your creativity", 
+	characters, logos, names and all related indicia are trademarks of
+	<a id="_ctl0_MasterFooter_HyperLink2" class="Legal" href="info/About.aspx">ROBLOX Corporation</a>, 
+	
+	2006
+	. Patents pending.
+	<br>
+	<a id="_ctl0_MasterFooter_HyperLink3" class="Legal" href="info/Privacy.aspx">Privacy Policy</a>&nbsp;|
+	<a href="http://web.archive.org/web/20060703125908/mailto:info@roblox.com" class="Legal">Contact Us</a>&nbsp;|
+	<a id="_ctl0_MasterFooter_HyperLink5" class="Legal" href="info/About.aspx">About Us</a><br>
+	Use of this site signifies your acceptance of the
+	<a id="_ctl0_MasterFooter_HyperLink1" class="Legal" href="info/TermsOfService.aspx">Terms and Conditions</a></div>
+
+                    </td>
+                </tr>
+            </table>
+        </div>
+        
+    
+<input type="hidden" name="__VIEWSTATEENCRYPTED" id="__VIEWSTATEENCRYPTED" value=""/>
+<input type="hidden" name="__EVENTVALIDATION" id="__EVENTVALIDATION" value="F1OBgiJo1O9SFvzUnZny2xCoesxmeeiMeFcD0O8Tycu+N9dQJWkAj76rIldDtWZQlhaJ6+h9XPyP1eqiP6hR2A=="/>
+<script language="javascript" type="text/javascript">window.document.body.topMargin = 0;</script></form>
+</body>
+</html>
